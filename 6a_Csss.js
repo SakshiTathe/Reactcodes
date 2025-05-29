f1936456d5f25834dd7ed895e6918f51f26b8aec
SakshiTathe <sakshitathe2004@gmail.com>
2025-05-29 22:02:40 +0530

    Update 6a_Csss.js

--- 6a_Csss.js
+++ 6a_Csss.js
@@ -3,8 +3,26 @@ import logo from './logo.svg';
 import './card.css'
 
 export const Csss = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+ 
     return (
         <div>
+        <nav className="navbar">
+      <div className="navbar-logo">MySite</div>
+      <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
+        <a href="#" className="nav-link">Home</a>
+        <a href="#" className="nav-link">About</a>
+        <a href="#" className="nav-link">Services</a>
+        <a href="#" className="nav-link">Contact</a>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
+    </nav>
             <div className='card'>
                 <div style={{display:'flex',flexDirection:'column'}}>
                     <img src={logo} alt='react' />
