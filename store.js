dc23ba7b6f2ccf6fcb66a93abe2eda99eafb5f53
SakshiTathe <sakshitathe2004@gmail.com>
2025-05-29 22:06:44 +0530

    Create store.js

---
+++ store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from 'redux';
+import counterReducer from './reducers/counterReducer';
+import todoReducer from './reducers/todoReducer';
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  todos: todoReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
