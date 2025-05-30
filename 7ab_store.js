b8adaa2baf6c7e3aa8a63cabad342604c8867103
SakshiTathe <sakshitathe2004@gmail.com>
2025-05-30 11:24:14 +0530

    Update 7ab_store.js

--- 7ab_store.js
+++ 7ab_store.js
@@ -1,3 +1,6 @@
+npm install react-redux
+npm install @reduxjs/toolkit react-redux
+
 import { createStore, combineReducers } from 'redux';
 import counterReducer from './reducers/counterReducer';
 import todoReducer from './reducers/todoReducer';
